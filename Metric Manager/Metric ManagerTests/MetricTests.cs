7082e7beed7cb22795fc5cb130f1764178bf7337
iftah <levyiftah@post.bgu.ac.il>
2022-03-13 16:01:49 +0200

    added test project for metric manager

---
+++ Metric Manager/Metric ManagerTests/MetricTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hype7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hype7.Tests
+{
+    [TestClass()]
+    public class MetricTests
+    {
+        [TestMethod()]
+        public void MetricTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
