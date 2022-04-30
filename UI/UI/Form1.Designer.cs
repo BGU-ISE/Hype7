82909fd48e6a22a35ffb49edfa34f6b4a08b3c8c
Lynn <lynnn@post.bgu.ac.il>
2022-04-30 13:02:45 +0300

    Issue #10 Started A new project, created side bar menu

---
+++ UI/UI/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace UI
+{
+    partial class MenuBtn
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuBtn));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnMetrics = new System.Windows.Forms.Button();
+            this.btnHomepage = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnMetrics);
+            this.panel1.Controls.Add(this.btnHomepage);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // SettingsBtn
+            // 
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("SettingsBtn.Image")));
+            this.btnSettings.Location = new System.Drawing.Point(0, 535);
+            this.btnSettings.Name = "SettingsBtn";
+            this.btnSettings.Size = new System.Drawing.Size(186, 42);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.BtnSettings_Click);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave);
+            // 
+            // btnMetrics
+            // 
+            this.btnMetrics.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMetrics.FlatAppearance.BorderSize = 0;
+            this.btnMetrics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMetrics.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMetrics.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnMetrics.Image = ((System.Drawing.Image)(resources.GetObject("btnMetrics.Image")));
+            this.btnMetrics.Location = new System.Drawing.Point(0, 186);
+            this.btnMetrics.Name = "btnMetrics";
+            this.btnMetrics.Size = new System.Drawing.Size(186, 42);
+            this.btnMetrics.TabIndex = 1;
+            this.btnMetrics.Text = "Metrics";
+            this.btnMetrics.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnMetrics.UseVisualStyleBackColor = true;
+            this.btnMetrics.Click += new System.EventHandler(this.BtnMetrics_Click);
+            this.btnMetrics.Leave += new System.EventHandler(this.btnMetrics_Leave);
+            // 
+            // btnHomepage
+            // 
+            this.btnHomepage.AccessibleName = "HomepageBtn";
+            this.btnHomepage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHomepage.FlatAppearance.BorderSize = 0;
+            this.btnHomepage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHomepage.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnHomepage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnHomepage.Image = ((System.Drawing.Image)(resources.GetObject("btnHomepage.Image")));
+            this.btnHomepage.Location = new System.Drawing.Point(0, 144);
+            this.btnHomepage.Name = "btnHomepage";
+            this.btnHomepage.Size = new System.Drawing.Size(186, 42);
+            this.btnHomepage.TabIndex = 0;
+            this.btnHomepage.Text = "Homepage";
+            this.btnHomepage.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnHomepage.UseVisualStyleBackColor = true;
+            this.btnHomepage.Click += new System.EventHandler(this.BtnHomepage_Click);
+            this.btnHomepage.Leave += new System.EventHandler(this.btnHomepage_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 193);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 1;
+            this.pnlNav.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // MenuBtn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.pnlNav);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MenuBtn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MenuBtn";
+            this.Load += new System.EventHandler(this.MenuBtn_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnHomepage;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnMetrics;
+        private System.Windows.Forms.Panel pnlNav;
+    }
+}
+
---
+++ UI/UI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace UI
+{
+    public partial class MenuBtn : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+         (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+
+         );
+
+        public MenuBtn()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+
+            pnlNav.Height = btnHomepage.Height;
+            pnlNav.Top = btnHomepage.Top;
+            pnlNav.Left = btnHomepage.Left;
+            btnHomepage.BackColor = Color.FromArgb(46, 51, 73);
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BtnMetrics_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnMetrics.Height;
+            pnlNav.Top = btnMetrics.Top;
+            pnlNav.Left = btnMetrics.Left;
+            btnMetrics.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void MenuBtn_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnHomepage_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnHomepage.Height;
+            pnlNav.Top = btnHomepage.Top;
+            pnlNav.Left = btnHomepage.Left;
+            btnHomepage.BackColor = Color.FromArgb(46, 51, 73);
+
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSettings.Height;
+            pnlNav.Top = btnSettings.Top;
+            pnlNav.Left = btnSettings.Left;
+            btnSettings.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnHomepage_Leave(object sender, EventArgs e)
+        {
+            btnHomepage.BackColor = Color.FromArgb(24, 30, 54);
+
+        }
+
+        private void btnMetrics_Leave(object sender, EventArgs e)
+        {
+            btnMetrics.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(24, 30, 54);
+        }
+    }
+}
