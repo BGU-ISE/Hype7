280258458f9d4cf833ce608ee016a476de6a1c4e
lynnn <lynnn@audiocodes.com>
2022-05-20 14:22:19 +0300

    Some comments and unnecessary code removed

--- UI/UI/Form1.cs
+++ UI/UI/Form1.cs
@@ -29,8 +29,6 @@ namespace UI
 
         //Fields
         private Button currentButton;
-        private Random random;
-        private int tempIndex;
         private Form activeForm;
 
         public MenuBtn()
@@ -60,40 +58,18 @@ namespace UI
             childForm.Show();
             lblTitle.Text = childForm.Text;
         }
+
         private void ActivateButton(object btnSender)
         {
             if (btnSender != null)
             {
                 if (currentButton != (Button)btnSender)
                 {
-                   // DisableButton();
-                    //Color color = SelectThemeColor();
                     currentButton = (Button)btnSender;
-                    //currentButton.BackColor = color;
-                    //currentButton.ForeColor = Color.White;
                     currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                    //panelTitleBar.BackColor = color;
-                   // panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
-                    //ThemeColor.PrimaryColor = color;
-                    //ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
-                  //  btnCloseChildForm.Visible = true;
                 }
             }
         }
-        /*
-        private void DisableButton()
-        {
-            foreach (Control previousBtn in panelMenu.Controls)
-            {
-                if (previousBtn.GetType() == typeof(Button))
-                {
-                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
-                    previousBtn.ForeColor = Color.Gainsboro;
-                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                }
-            }
-        }*/
-   
        
         private void Reset()
         {
@@ -129,7 +105,6 @@ namespace UI
             if (activeForm != null)
                 activeForm.Close();
             Reset();
-
         }
 
         private void BtnSettings_Click(object sender, EventArgs e)
